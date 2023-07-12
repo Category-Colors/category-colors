d771cfc70392d1129b238d318f62453d1c286b30
r1g023 <limon1285@gmail.com>
2023-07-12 00:18:33 -0500

    Add nav component within landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Grid } from "@chakra-ui/react";
+import Navigation from "./Navigation";
 
 function LandingPage() {
-  return <div>Landing Page test</div>;
+  return (
+    <Grid
+      gap={0}
+      templateAreas={`"navbar navbar  "
+              "leftUpperCard main "
+              "leftBottomCard main  "
+              `}
+      gridTemplateRows={"4.1% 2fr 50% 0px"}
+      gridTemplateColumns={"27% 1fr"}
+      color="blackAlpha.700"
+      fontWeight="bold"
+      w={"100wh"}
+      h={"100vh"}>
+      {/* Navigation */}
+      <Navigation />
+    </Grid>
+  );
 }
 
 export default LandingPage;
