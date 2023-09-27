0a4c3bd00a24bed54a6800a9af1dfd5af78b10b8
r1g023 <limon1285@gmail.com>
2023-09-27 00:34:34 -0500

    squashed - removed some button props, updated PalHeader, removed frontWeigh from LandingPage

---
+++ src/components/PaletteColors.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { Box, IconButton } from "@chakra-ui/react";
+
+function PaletteColors() {
+  return (
+    <Box
+      flex="5"
+      display="flex"
+      justifyContent={"flex-start"}
+      alignItems={"center"}
+      color={"#000000"}
+    >
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#054849"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#8ef08a"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#0081ec"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#f9c2f5"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#0ae2ff"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#f5be01"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#ff6c37"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+      <IconButton
+        isRound={true}
+        variant="solid"
+        background="#744012"
+        aria-label="Done"
+        fontSize="20px"
+        marginLeft="8px"
+      />
+    </Box>
+  );
+}
+
+export default PaletteColors;
--- src/components/PaletteMainCard.js
+++ src/components/PaletteMainCard.js
@@ -1,10 +1,20 @@
 import { Box } from "@chakra-ui/react";
-import { Heading } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import PaletteColors from "./PaletteColors";
+import PaletteCopyButton from "./PaletteCopyButton";
+import PaletteHeader from "./PaletteHeader";
 
 function PaletteMainCard() {
   return (
-    <Box gridArea="main" bg="white.500" borderLeft={"1px solid gray"} p={4}>
-      <Heading size="md">Palette #200123</Heading>
+    <Box gridArea="main" borderLeft={"1px solid gray"} p={4}>
+      <Flex color="#000000">
+        {/* Palette Header with date and id # */}
+        <PaletteHeader />
+        {/* Palette Color Circle Buttons */}
+        <PaletteColors />
+        {/* Palette Copy Button to copy hex values to clipboard */}
+        <PaletteCopyButton />
+      </Flex>
     </Box>
   );
 }
