9d8f58a4ae60a030e61fab8028b6393982d62021
r1g023 <limon1285@gmail.com>
2023-09-27 00:34:34 -0500

    squashed - removed some button props, updated PalHeader, removed frontWeigh from LandingPage

---
+++ src/components/PaletteHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
+
+function PaletteHeader() {
+  return (
+    <Box flex="2">
+      <Heading size="lg">Palette #200123</Heading>
+      <Text fontSize={14} sx={{ lineHeight: "21px" }} color="gray">
+        Generated June 7, 2023 at 5:42pm
+      </Text>
+    </Box>
+  );
+}
+
+export default PaletteHeader;
